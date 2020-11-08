a0f93cd0f624ad8d6c94c0331782a943f1c098ce
julenalzas <julenandhernandez@gmail.com>
2020-11-08 19:16:16 +0100

    Azken aldaketak

--- src/factory/Main.java
+++ src/factory/Main.java
@@ -2,11 +2,13 @@ package factory;
 
 import domain.Covid19Pacient;
 import domain.Medicament;
+import domain.SymptomFactory;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Covid19Pacient p1=new Covid19Pacient("aitor", 35);
+		SymptomFactory factory= new SymptomFactory();
+		Covid19Pacient p1=new Covid19Pacient("aitor", 35,factory);
 		new PacientSymptomGUI(p1);
 
 	}
--- src/observer/Main.java
+++ src/observer/Main.java
@@ -3,6 +3,7 @@ package observer;
 import java.util.Observable;
 
 import domain.Covid19Pacient;
+import domain.SymptomFactory;
 
 public class Main {
 
@@ -10,11 +11,30 @@ public class Main {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		Observable pacient=new Covid19Pacient("aitor", 35);
+		SymptomFactory factory= new SymptomFactory();
+//		Observable pacient=new Covid19Pacient("aitor", 35,factory);
+//		PacientObserverGUI pacientGUI= new PacientObserverGUI (pacient);
+//		PacientSymptomGUI frame = new PacientSymptomGUI ((Covid19Pacient) pacient);
+//		
+//		Observable pacient_julen=new Covid19Pacient("julen", 20,factory);
+//		PacientObserverGUI pacient_julenGUI= new PacientObserverGUI (pacient_julen);
+//		PacientSymptomGUI framepacient_julen = new PacientSymptomGUI ((Covid19Pacient) pacient_julen);
+		
+		Observable pacient=new Covid19Pacient("aitor", 35,factory);
 		PacientObserverGUI pacientGUI= new PacientObserverGUI (pacient);
 		PacientSymptomGUI frame = new PacientSymptomGUI ((Covid19Pacient)pacient);
 		PacientThermometerGUI thermometer= new PacientThermometerGUI((Covid19Pacient)pacient);
-		SemaphorGUI semaforoa= new SemaphorGUI((Covid19Pacient)pacient);
+		
+		Observable pacientjulen=new Covid19Pacient("julen", 21,factory);
+		PacientObserverGUI pacientGUIjulen= new PacientObserverGUI (pacientjulen);
+		PacientSymptomGUI framejulen = new PacientSymptomGUI ((Covid19Pacient)pacientjulen);
+		PacientThermometerGUI thermometerjulen= new PacientThermometerGUI((Covid19Pacient)pacientjulen);
+		
+		Observable pacientalain=new Covid19Pacient("alain", 20,factory);
+		PacientObserverGUI pacientGUIalain= new PacientObserverGUI (pacientalain);
+		PacientSymptomGUI framealain = new PacientSymptomGUI ((Covid19Pacient)pacientalain);
+		PacientThermometerGUI thermometeralain= new PacientThermometerGUI((Covid19Pacient)pacientalain);
+//		SemaphorGUI semaforoa= new SemaphorGUI((Covid19Pacient)pacient);
 		
 //		Observable pacient_julen=new Covid19Pacient("julen", 21);
 //		PacientObserverGUI pacientGUI_julen= new PacientObserverGUI (pacient_julen);
